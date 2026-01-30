d6d7f42cd7c97dee610868cedc63a8d421f1fa6b
Kishore.V <113716171+Master-kishore@users.noreply.github.com>
2026-01-30 21:02:31 +0530

    checking

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
      <div>
-      <h1>Get Ready guys</h1>
+      <h1>Get Ready guys..???</h1>
      </div>
     </div>
   );
